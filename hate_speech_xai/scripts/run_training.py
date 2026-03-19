6bd248f7c2d193bcbe0280e1d9ebb4cebe54858a
Jessica Hassibi <j.j.hassibi@gmail.com>
2026-03-19 14:01:03 +0100

    Catch FileNotFoundError

--- hate_speech_xai/scripts/run_training.py
+++ hate_speech_xai/scripts/run_training.py
@@ -5,6 +5,7 @@ from hate_speech_xai.src.models.train import train_transformer
 def main():
     train, val, _ = load_preprocessed_dataset()
     train, val = tokenize_dataset(train, val)
+
     train_transformer(train, val)
 
 if __name__ == "__main__":
