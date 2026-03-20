c89b9a60e69ea5b699865796be0b18077a4f092e
Jessica Hassibi <j.j.hassibi@gmail.com>
2026-03-20 10:54:04 +0100

    Add configuration v2

--- hate_speech_xai/config.py
+++ hate_speech_xai/config.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 SAVED_MODELS_DIR = Path(BASE_DIR) / "models/hatexplain_classifier"
+SAVED_MODELS_V2_DIR = Path(BASE_DIR) / "models/hatexplain_classifier_v2"
 CHECKPOINT_DIR = Path(BASE_DIR) / "checkpoints"
+CHECKPOINT_V2_DIR = Path(BASE_DIR) / "checkpoints_v2"
 APP_DATA_DIR = Path(BASE_DIR) / "app/data"
 PREPROCESSED_DATA_DIR = Path(BASE_DIR) / "data/preprocessed"
 
@@ -34,3 +36,20 @@ TRAINING_ARGS = {
     "metric_for_best_model": "f1",
     "save_total_limit": 2
 } # using standard huggingface arguments
+
+TRAINING_ARGS_V2 = {
+	"output_dir": CHECKPOINT_V2_DIR,
+    "num_train_epochs": 8,
+    "per_device_train_batch_size": 32,
+    "per_device_eval_batch_size": 32,
+    "logging_strategy": "steps",
+    "logging_steps": 50,
+    "learning_rate": 2e-5,
+    "warmup_ratio": 0.1,
+    "weight_decay": 0.01,
+    "eval_strategy": "epoch",
+    "save_strategy": "epoch",
+    "load_best_model_at_end": True,
+    "metric_for_best_model": "f1",
+    "save_total_limit": 2
+}
